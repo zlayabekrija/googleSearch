843271729ae78e022edd8f00eba58d287ec07191
zlajabekrija <zlaja.bekrija@gmail.com>
2018-10-23 15:25:03 +0200

    final lines

---
+++ Readme2.md
@@ -0,0 +1,5 @@
+This are things I have dose so far:
+
+-added alert on buttons 'search' and 'I'm feeling lucky';
+-on logo hover added text 'Are you looking for me';
+-on form submit popup gets updated with input from search field
--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,22 @@
+//function that gives alert of search term
+function look() {
+	let getValue=document.getElementById('form').elements[0].value;
+	alert('Wait a second we will look for ' +"'"+ getValue+"'" + ' in seconds.');
+	
+}
+//added alert to I'm feeling lucky button
 function random() {
 	alert('Somenthing funny');
+}
+//added placeholder on google hover 
+function test () {
+	let google = 'Are you looking for me ?';
+	let input = document.getElementById('seek');
+	input.placeholder = google;
+}
+//reset placeholder on mouse out of logo
+function out () {
+	let input = document.getElementById('seek');
+	let place = 'Look for something';
+	input.placeholder = place;
 }
